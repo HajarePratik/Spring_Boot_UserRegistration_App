0d6a7e2edb7615a1dd66f4bb10e0cfcfddbe359a
HajarePratik <pratikhajare09@gmail.com>
2021-08-26 09:46:38 +0530

    [Pratik]Add : Added Dependency and Config Project for Eureka

--- src/main/java/com/bridgelabz/userregistration/SpringBootUserAppApplication.java
+++ src/main/java/com/bridgelabz/userregistration/SpringBootUserAppApplication.java
@@ -2,12 +2,14 @@ package com.bridgelabz.userregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.bridgelabz.userregistration")
 @EnableJpaRepositories("com.bridgelabz.userregistration.respository")
+@EnableEurekaClient
 public class SpringBootUserAppApplication {
 
 	public static void main(String[] args) {
